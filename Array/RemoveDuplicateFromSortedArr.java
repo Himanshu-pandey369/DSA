bd6ec484924b58e8131e84bf2b6898397110e476
Himanshu <hp1981388@gmail.com>
2026-06-26 09:16:46 +0530

    Remove Duplicaate From Sorted Array problem solved

---
+++ Array/RemoveDuplicateFromSortedArr.java
@@ -0,0 +1,22 @@
+public class RemoveDuplicateFromSortedArr {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int i = 0; 
+
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1; 
+    }
+    public static void main(String[] args) {
+        int[] nums ={1,2,3,3,4};
+        RemoveDuplicateFromSortedArr obj = new RemoveDuplicateFromSortedArr();
+        int result = obj.removeDuplicates(nums);
+        System.out.println(result);
+    }
+}
