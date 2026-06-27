97e82eb847225e2438f79ec759cb3a4e2fa1bd2f
Himanshu <hp1981388@gmail.com>
2026-06-27 09:06:59 +0530

    Swap Nodes In Pairs problem solved

---
+++ LinkedList/SwapNodesInPairs.java
@@ -0,0 +1,31 @@
+public class SwapNodesInPairs {
+    ListNode SwapNodesInPairs(ListNode head){
+        if(head==null||head.next==null){
+            return head;
+        }
+        ListNode first = head;
+        ListNode sec = head.next;
+        ListNode prev = null;
+        while (first!=null && sec!=null) {
+            ListNode third=sec.next;
+            sec.next =first;
+            first.next = third;
+            if(prev!=null){
+                prev.next=sec;
+            }else{
+                head =sec;
+            }
+            prev=first;
+            first=third;
+            if(third!=null){
+                sec=third.next;
+            }else{
+                sec=null;
+            }
+        }
+        return head;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
