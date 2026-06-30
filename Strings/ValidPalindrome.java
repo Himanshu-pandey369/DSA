3cbace8e2a63e82fda52b94295ade635f20ead0e
Himanshu <hp1981388@gmail.com>
2026-06-30 18:31:50 +0530

    .

--- Strings/ValidPalindrome.java
+++ Strings/ValidPalindrome.java
@@ -1,6 +1,6 @@
 public class ValidPalindrome {
     boolean alphanumeric(char ch) {
-        if ((ch >= 'a' && ch <= 'z') ||
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') ||
                 (ch >= '0' && ch <= '9')) {
             return true;
         }
