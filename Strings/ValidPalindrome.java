9b5e8533e08f109ca8f3c21902af74dc1453e022
Himanshu <hp1981388@gmail.com>
2026-06-30 18:20:04 +0530

    Valid Palindrome Problems solution

---
+++ Strings/ValidPalindrome.java
@@ -0,0 +1,42 @@
+public class ValidPalindrome {
+    boolean alphanumeric(char ch) {
+        if ((ch >= 'a' && ch <= 'z') ||
+                (ch >= 'A' && ch <= 'Z') ||
+                (ch >= '0' && ch <= '9')) {
+            return true;
+        }
+        return false;
+    }
+
+    boolean Checkvalid(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+
+            if (!alphanumeric(s.charAt(i))) {
+                i++;
+                continue;
+            }
+
+            if (!alphanumeric(s.charAt(j))) {
+                j--;
+                continue;
+            }
+
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ValidPalindrome obj = new ValidPalindrome();
+        System.out.println(obj.Checkvalid("A man, a plan, a canal: Panama"));
+    }
+}
