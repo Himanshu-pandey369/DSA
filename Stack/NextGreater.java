02b81cf8bd5b6da135d8787347406bbbd6b15e36
Himanshu <hp1981388@gmail.com>
2026-07-31 09:02:02 +0530

    Next Greater element problem solved

---
+++ Stack/NextGreater.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class NextGreater {
+
+    public static int[] nextGreaterElement(int[] arr) {
+        int n = arr.length;
+        int[] result = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                result[i] = -1;
+            } else {
+                result[i] = stack.peek();
+            }
+            stack.push(arr[i]);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 2, 10, 8};
+
+        int[] ans = nextGreaterElement(arr);
+
+        System.out.println(Arrays.toString(ans));
+    }
+}
